a00d8a34510072c5ffb55c7178059e69e37f0b3b
TKMhub <111867865+TKMhub@users.noreply.github.com>
2023-06-10 21:46:34 +0900

    style

---
+++ components/Header/page.tsx
@@ -0,0 +1,52 @@
+"use client";
+import Link from "next/link";
+import styles from "./header.module.scss";
+import Image from "next/image";
+import TopLogo from "public/codeParts_Black.svg";
+import { Button } from "@mui/material";
+
+const Header = () => {
+  return (
+    <div className={styles.Header}>
+          <Link href="/">
+            <Image src={TopLogo} style={{ width: "100px" }} alt="Logo" priority={true} />
+          </Link>
+          <div className={styles.Header_right}>
+            <div className={styles.Header_right_item}>
+              <Link href="/" className={styles.Header_right_item_link}>
+                TOP
+              </Link>
+              <Link href="/" className={styles.Header_right_item_link}>
+                Blog
+              </Link>
+              <Link href="/" className={styles.Header_right_item_link}>
+                Skills
+              </Link>
+              <Link href="/" className={styles.Header_right_item_link}>
+                Contact
+              </Link>
+              <Link href="/" className={styles.Header_right_item_buttonSpace}>
+                <Button
+                  className={styles.Header_right_item_buttonSpace_button}
+                  variant="contained"
+                  style={{ width: "80px", height: "40px" }}
+                >
+                  Login
+                </Button>
+              </Link>
+              <Link href="/" className={styles.Header_right_item_buttonSpace}>
+                <Button
+                  className={styles.Header_right_item_buttonSpace_button}
+                  variant="contained"
+                  style={{ width: "80px", height: "40px" }}
+                >
+                  新規
+                </Button>
+              </Link>
+            </div>
+          </div>
+        </div>
+  )
+}
+
+export default Header
